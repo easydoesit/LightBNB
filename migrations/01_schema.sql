d207636a351f74d3efa9479fbc6ac33b3025ecbd
easydoesit <michael.grills@gmail.com>
2023-01-19 04:04:53 +0000

    add seed data

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE properties (
   cover_photo_url VARCHAR(255) NOT NULL,
   cost_per_night INTEGER NOT NULL DEFAULT 0,
   parking_spaces INTEGER NOT NULL DEFAULT 0,
-  number_of_bathrroms INTEGER NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
