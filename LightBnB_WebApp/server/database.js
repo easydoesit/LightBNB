7a8ab44c84769485a8a5b530c3c72e89f9489092
easydoesit <michael.grills@gmail.com>
2023-01-26 07:27:55 +0000

    refactor getAllproperties to work in various scenarios

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -88,8 +88,67 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, AVG(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id`;
+  if (options.owner_id || options.city || options.maximum_price_per_night || options.minimum_price_per_night) {
+    queryString += `
+    WHERE `;
+  }
+  
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `owner_id = $${Number(queryParams.length)}`;
+  }
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `city LIKE $${queryParams.length}`;
+  }
+
+  if (options.minimum_price_per_night) {
+    if (queryParams.length > 0) {
+      queryString += ' AND ';
+    }
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+    queryString += `cost_per_night >= $${queryParams.length}`;
+
+  }
+
+  if (options.maximum_price_per_night) {
+    if (queryParams.length > 0) {
+      queryString += ' AND ';
+    }
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    queryString += `cost_per_night <= $${(queryParams.length)}`;
+  
+  }
+
+  queryString += `
+  GROUP BY properties.id`;
+
+  if (options.minimum_rating) {
+    
+    queryParams.push(`${options.minimum_rating}`);
+    
+    queryString += `
+    HAVING AVG(property_reviews.rating) >= $${(queryParams.length)} `;
+  
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(queryString, queryParams)
     .then((result) => {
       return result.rows;
     })
